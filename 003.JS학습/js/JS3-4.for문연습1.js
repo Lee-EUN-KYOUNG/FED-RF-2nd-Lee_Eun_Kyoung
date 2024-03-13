a7c1df6f863116180aa6429e3ef82083a733ec7e
EUNKYOUNG <amurca@naver.com>
2024-03-13 12:07:45 +0900

    23회차

--- 003.JS학습/js/JS3-4.for문연습1.js
+++ 003.JS학습/js/JS3-4.for문연습1.js
@@ -31,15 +31,15 @@
 //  1. 대상 선정
 // 1-1. 이벤트 대상
 // (1) 미니언즈 버튼 (3개)
-const mini = document.querySelectorAll('.mini');
+const mini = document.querySelectorAll(".mini");
 // (2) 리셋 버튼 (1)
-const rbtn = document.querySelector('.rbtn');
+const rbtn = document.querySelector(".rbtn");
 
 // 1-2. 변경 대상
 // (1) 파란 박스 (미니언즈 내부 박스)
-const Bcase = document.querySelector('.mini-space');
+const Bcase = document.querySelector(".mini-space");
 // (2) 갯수 출력 박스
-const stxt = document.querySelector('.stxt span');
+const stxt = document.querySelector(".stxt span");
 
 //console.log('대상:', mini,rbtn,Bcase,stxt);
 
@@ -49,18 +49,24 @@ const stxt = document.querySelector('.stxt span');
 // 0부터 시작하여 미니언즈 개수보다 작을때까지 반복
 // -> 왜 0부터? : HTML 컬렉션이 0부터 시작하기 때문 (유사 배열)
 // -> mini.length는 HTML 컬렉션의 개수를 리턴함
-for (let i=0; i<mini.length; i++) {
-    // 대상 : mini.item(순번) 또는 mini[순번]
-    // mini[i].onclick = () => {};
-    // 이벤트 리스너를 사용한 사용한 방법도 있음 : mini[i].addEventListener('click', insertMini);
-    mini[i].onclick = insertMini;
+for (let i = 0; i < mini.length; i++) {
+  // 대상 : mini.item(순번) 또는 mini[순번]
+  // mini[i].onclick = () => {};
+  // 이벤트 리스너를 사용한 사용한 방법도 있음 : mini[i].addEventListener('click', insertMini);
+  mini[i].onclick = insertMini;
 
-/* console.log(`
+  /* console.log(`
     for문 내부에 전달된 i변수값: ${i}
     \n
     순서대로 미니언즈 대상확인: ${mini[i]}
 `);  */
 } /// for문///
+
+// 3. 기능 구현 함수
+// 함수에서 전역 변수로 사용할 미니언즈 개수 저장 변수
+let count = 0;
+
+
 
 /******************************************************** 
         함수명 : insertMini
@@ -68,12 +74,31 @@ for (let i=0; i<mini.length; i++) {
 ********************************************************/
 
 function insertMini() {
-    // 1. 함수 호출 확인
-    console.log('미니언즈 드루와~~!');
+  // this 키워드는 함수에 연결된 요소 자신! (.mini)
 
-    // 2. 변경대상선정 : .mini-space -> Bcase 변수
-    // 3. 변경 내용 적용하기 : html 넣기
-    // += 대입 연산자로 기존값에 더함!
+  // 0. 이미지에 셋팅된 'data-num' 속성값 가져오기
+  let num = this.getAttribute("data-num");
+  // getAttribute(속성명) -> 속성값 읽어오기 내장 함수
+  // setAttribute(속성명,값) -> 속성값 넣기 내장함수
+
+  // 1. 함수 호출 확인
+  console.log("미니언즈 드루와~~!", num);
+
+  // 2. 변경대상선정 : .mini-space -> Bcase 변수
+
+  // 3. 변경 내용 적용하기 : html 넣기
+  // += 대입 연산자로 기존값에 더함!
+  // for문으로 전달될 개수만큼 반복하여 이미지 넣는다
+  for (let i = 0; i < num; i++) {
     Bcase.innerHTML += `<img src="./images/Minions.png" alt="미니언즈 이미지">`;
+
+    //미니언즈 이미지 개수 증가하기
+    count++;
+  }
+
+  // 4. 미니언즈 개수 출력하기
+  // 대상 : .stxt span -> stxt 변수
+  // 미니언즈 이미지에 3명의 미니언즈가 있으므로 3을 곱하여 출력한다!
+  stxt.innerText = count*3;
 
 } /////////// insertMini 함수 ///////////////
