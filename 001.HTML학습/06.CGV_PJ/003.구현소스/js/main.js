5ae1913b1b859f815ff60e8324c2dc3ed855a12d
EUNKYOUNG <amurca@naver.com>
2024-03-25 12:17:48 +0900

    Update main.js

--- 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
@@ -14,17 +14,33 @@ const pMenu = document.querySelectorAll('.poster-menu a');
 const ifr = document.querySelector('#ifr');
 console.log('대상:',pMenu,ifr);
 
-// 2. 이벤트 설정 및 기능 구현
+// 2. 영화 아이디 정보 객체로 구성하기
+const movieId = {
+    "파묘":"fRkOWmfZjkY",
+    "웡카":"Bldf9SWRPFM",
+    "이프온리":"8TQx6nLopq4",
+    "소풍":"7VHsScXQyw0",
+    "시민덕희":"dW8klYbV5t0",
+    "바튼아카데미":"DNUF8Mt0svk",
+}
+
+// 3. 이벤트 설정 및 기능 구현
 // 포스터 버튼에 forEach() 메서드로 순회한다
 pMenu.forEach((ele)=>{
     ele.onclick = ()=>{
     // 클릭된 a 요소를 구분하기위해 하위 img 포스터의 alt 속성 읽어오기
     // 속성읽기 내장함수 : getAttribute('속성명');
     let txt = ele.querySelector('img').getAttribute('alt');
+    console.log('나클릭!',txt)
 
-
-    console.log('나클릭!',ele);
-
+    // 아이프레임 src 변경하기
+    // 속성 변경 JS 내장 함수 -> setAttribute(속성명,값);
+    // 대상 : 아이프레임(#ifr -> ifr 변수)
+    // 영화 아이디 값 : movieId 객체
+    // 객체 호출법 : movieId[영화이름 속성명]
+    // 영화이름 속성명은 txt 변수에 할당되어 있음
+    // 객체 호출 코드 : movieId[txt]
+    ifr.setAttribute('src',`https://www.youtube.com/embed/${movieId[txt]}?autoplay=1`);
 
     }; ///////// click함수
 
