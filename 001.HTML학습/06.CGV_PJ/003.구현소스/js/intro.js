26731b2fa888f42476517c36a2617b5b8030b1fa
EUNKYOUNG <amurca@naver.com>
2024-04-01 12:53:15 +0900

    35화

--- 001.HTML학습/06.CGV_PJ/003.구현소스/js/intro.js
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/intro.js
@@ -1,1 +1,20 @@
 // CGV PJ 인트로 페이지 JS - intro.js
+
+// 요구 사항 : 비디오 플레이가 끝나면 쳇페이지인
+// main.html로 자동 이동하기
+
+// 대상 : #myvid
+const myvid = document.querySelector('#myvid');
+
+// 이벤트 :  timeuotate -> 동영상 재생중 발생 이벤트
+myvid.addEventListener('timeupdate',() => {
+    
+    // 1. 동영상 멈춤 여부 알아내기 : 비디오 요소.paused
+    // => 멈춤 상태면 true, 아니면 false
+    let isStop = myvid.paused;
+
+    // 호출 확인
+    console.log('재생중!',isStop);
+
+
+}); /////////////////////// timeuotate 이벤트 함수 //////////////////////////
