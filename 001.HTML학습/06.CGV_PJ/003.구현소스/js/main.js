b4d6d88d76bc9f0f05ea645ae18740c21f11c9db
EUNKYOUNG <amurca@naver.com>
2024-04-01 10:59:15 +0900

    36회차

--- 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
@@ -12,7 +12,7 @@
 const pMenu = document.querySelectorAll('.poster-menu a');
 // 1-2. 변경 대상 : #ifr
 const ifr = document.querySelector('#ifr');
-console.log('대상:',pMenu,ifr);
+// console.log('대상:',pMenu,ifr);
 
 // 2. 영화 아이디 정보 객체로 구성하기
 const movieId = {
@@ -26,12 +26,14 @@ const movieId = {
 
 // 3. 이벤트 설정 및 기능 구현
 // 포스터 버튼에 forEach() 메서드로 순회한다
-pMenu.forEach((ele)=>{
+pMenu.forEach((ele,idx)=>{
+    // ele는 각 a요소, idx는 각 요소의 순번
+
     ele.onclick = ()=>{
     // 클릭된 a 요소를 구분하기위해 하위 img 포스터의 alt 속성 읽어오기
     // 속성읽기 내장함수 : getAttribute('속성명');
     let txt = ele.querySelector('img').getAttribute('alt');
-    console.log('나클릭!',txt)
+    // console.log('나클릭!',txt)
 
     // 아이프레임 src 변경하기
     // 속성 변경 JS 내장 함수 -> setAttribute(속성명,값);
@@ -41,7 +43,20 @@ pMenu.forEach((ele)=>{
     // 영화이름 속성명은 txt 변수에 할당되어 있음
     // 객체 호출 코드 : movieId[txt]
     ifr.setAttribute('src',`https://www.youtube.com/embed/${movieId[txt]}?autoplay=1`);
+    
+    // 클릭된 a의 부모인 li에 클래스 on 넣기
+    pMenu.forEach((x,i)=>{
+     // x는 a요소, i는 순번
+     // x.parentElement는 a요소 상위 부모 li 요소
+     if(i===idx){
+        x.parentElement.classList.add("on");
+     } ////// if
+
+     else{
+        x.parentElement.classList.remove("on");
+     } //// else
+    });  ///// forEach 메서드 사용
 
     }; ///////// click함수
 
-}); /////// forEahc /////////
+}); /////// forEach /////////
