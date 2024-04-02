e32106b9ad1372ab11fa7e38f3be4abc863696fe
EUNKYOUNG <amurca@naver.com>
2024-04-02 12:19:46 +0900

    Update main3.js

--- 001.HTML학습/07.ShoppingMall_PJ/js/main3.js
+++ 001.HTML학습/07.ShoppingMall_PJ/js/main3.js
@@ -59,8 +59,6 @@ function loadFn() {
   // 슬라이드 순번 전역변수
   let snum = 0;
 
-
-
   //////////// 초기셋팅하기 ////////
   // 5개의 슬라이드와 블릿을 만들어준다!
   for (let i = 0; i < SLIDE_CNT; i++) {
@@ -81,8 +79,6 @@ function loadFn() {
     `;
   } ////// for ////////
 
-
-
   // li를 생성한 후 그 li다시 수집한다!
   // (1) 슬라이드의 li까지 수집! slide 변수
   slide = qsa("#slide li");
@@ -90,16 +86,12 @@ function loadFn() {
   // (2) 블릿의 li까지 수집! indic 변수
   indic = qsa(".indic li");
 
-
   // 1. 이벤트 연결 설정하기
   // 대상 : .abtn
   // addEvt(대상,이벤트,함수);
   abtn.forEach((ele) => {
     addEvt(ele, "click", goSlide);
   });
-  
-
-
 
   // 2. 이벤트 처리 함수 만들기
   //  [처리 순서]
@@ -111,14 +103,17 @@ function loadFn() {
   // (6) 자동 넘김 구성 함수를 호출하여 인터발 호출 작동 -> 버튼 클릭시 인터발 지우기/일정시간 뒤 다시 인터발 작동
 
   function goSlide() {
-
     // 광클 금지 설정
-    if(stop) return;  // 막기
-    stop = true;  // 잠금
+    if (stop) return; // 막기
+    stop = true; // 잠금
     setTimeout(() => {
       stop = false; // 해제
     }, SLIDE_TRANS_TIME);
     ////////////////////////
+
+
+    // 0. 인터발 지우기 함수 호출
+    clearAuto();
 
     // 1. 오른쪽 버튼 여부
     let isRbtn = this.classList.contains("ab2");
@@ -137,46 +132,70 @@ function loadFn() {
     console.log("snum:" + snum);
 
     // 3. 슬라이드 순번 클래스 제어 함수 호출하기
-    setClass(slide,'on',snum)
+    setClass(slide, "on", snum);
 
     // 4. 블릿 순번 클래스 제어 함수 호출하기
-    setClass(indic,'on',snum);
-
+    setClass(indic, "on", snum);
 
   } ///////////////// goSlide 함수 /////////////
 
-
-
-
-
   // 3. 클래스 제어 함수 만들기
-  function setClass(target,className,seq) {
-
+  function setClass(target, className, seq) {
     // targer -  변경할 요소 대상
     // className -  변경할 클래스명
     // seq -  클래스가 들어갈 순번
 
-    console.log('대상:',target, '/클래스명:',className, '/순번',seq);
+    console.log("대상:", target, "/클래스명:", className, "/순번", seq);
 
     // 타겟은 HTML 컬렉션이므로 forEach로 순회함!
-    target.forEach((ele,idx) => {
+    target.forEach((ele, idx) => {
       // 1-1. seq와 idx가 일치할 경우 클래스 넣기
-      if(seq === idx) ele.classList.add(className);
-
+      if (seq === idx) ele.classList.add(className);
       // 1-2. 기타의 경우 클래스 제거
       else ele.classList.remove(className);
-
     }); //////////////////// forEach ////////////////
-
-
-
-
-
-
   } //////// setClass 함수
 
+  // [ 자동넘김 셋팅 구역 ] //////////////
+  // 인터발용 변수(지울목적)
+  let autoI;
+  // 타임아웃용 변수(지울목적)
+  let autoT;
+  // 자동넘김호출함수 최초호출하기
+  autoSlide();
 
+  // [ 자동넘김호출함수 ] /////
+  function autoSlide() {
+    // setInterval(함수,시간)
+    // - 일정시간간격으로 함수를 호출
+    // clearInterval(인터발변수)
+    // - 변수에 담긴 인터발을 지움(멈춤)
+    autoI = setInterval(() => {
 
+      // 현자 슬라이드 순번 증가
+      snum === SLIDE_CNT - 1 ? (snum = 0) : snum++;
 
+      // 슬라이드 순번 클래스 제어 함수 호출
+      setClass(slide, "on", snum);
+
+      // 블릿 순번 클래스 제어 함수 호출
+      setClass(indic, "on", snum);
+    }, 3000);
+  } /////// autoSlide 함수 /////////////
+
+  /// [ 인터발 지우기함수 : 버튼 조작시 호출함 ] ///////
+  function clearAuto() {
+    // 지우기 확인!
+    console.log("인터발 지워!");
+    // 1.인터발 지우기
+    clearInterval(autoI);
+    // 2.타임아웃 지우기 : 실행쓰나미 방지!!!
+    clearTimeout(autoT);
+    // 3.5초후 아무작동도 안하면 다시 인터발호출
+    autoT = setTimeout(() => {
+      autoSlide();
+    }, 5000);
+
+  } ////////// clearAuto ////////////
 
 } /////////////// loadFn 함수 //////////////
