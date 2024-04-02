3c05f36375ae40ef457473829b9748ef1ca276b5
EUNKYOUNG <amurca@naver.com>
2024-04-02 14:59:10 +0900

    37회차

---
+++ 003.JS학습/01.JS실험실/js/scroll_action.js
@@ -0,0 +1,70 @@
+/* JS실험실 : 04.스크롤액션 JS */
+
+// DOM 함수 객체 //////////////
+const myFn = {
+  // 요소선택함수 ////////
+  qs: (x) => document.querySelector(x),
+  qsEl: (el, x) => el.querySelector(x),
+  qsa: (x) => document.querySelectorAll(x),
+  qsaEl: (el, x) => el.querySelectorAll(x),
+
+  // 이벤트셋팅함수
+  addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
+}; /////// myFn 객체 /////////////
+
+// 1. 대상 선정 ///////////////////////
+
+// 2. 함수 호출 하기 ////////
+// 2-1. 글자 등장 함수 호출하기
+showLetters();
+
+
+// 3. 함수 만들기 /////////////////////
+
+
+
+
+//////////// 글자 등장 셋팅하기
+// 글자 등장 함수
+
+function showLetters() {
+  // 1. 대상선정 : .stage
+  const stage = myFn.qs(".stage");
+  console.log("대상:", stage);
+
+  // 3. 글자 데이터 변수 할당하기
+  const myText = "신카이 마코토";
+
+  // 4. 데이터 글자 한글자씩 태그로 싸기
+  // for of 사용!
+
+  // HTML 태그 변수
+  let hcode = "";
+
+  // 지연 시간 계산을 위한 순번 변수
+  let seqNum = 0;
+
+  for (let x of myText) {
+    if (x === " ") {
+      hcode += "&nbsp;&nbsp;";
+    } //////////// if /// 스페이스 공백 처리
+    else {
+      hcode += `
+    <span style="transition-delay: ${seqNum * 0.12}s"
+    >${x}</span>`;
+    } ///// else /////  글자일 경우 span태그 랩핑 처리
+
+    /// 중요!! -> 지연 시간에 곱해질 순번 증가하기
+    seqNum++;
+
+    // console.log(x);
+  } //////////// for of /////////////////
+
+  // 5. 스테이지에 코드 출력하기
+  stage.innerHTML = hcode;
+
+  // 6. 일정 시간 뒤 등장 클래스 .on 넣기
+  setTimeout(() => {
+    stage.classList.add("on");
+  }, 2000);
+} /////////// showLetters
