fe8f9209c13f7f07c01b8809d472156070576840
EUNKYOUNG <amurca@naver.com>
2024-04-02 11:20:19 +0900

    Update main3.js

--- 001.HTML학습/07.ShoppingMall_PJ/js/main3.js
+++ 001.HTML학습/07.ShoppingMall_PJ/js/main3.js
@@ -35,22 +35,27 @@ addEvt(window, "DOMContentLoaded", loadFn);
     기능: 로딩후 이벤트설정 및 슬라이드 기능
 ******************************************/
 function loadFn() {
-  console.log("로딩완료!");
+  // console.log("로딩완료!");
 
   // 이동버튼 대상:  .abtn
   const abtn = qsa(".abtn");
   // 변경대상 : #slide
   const slide = qs("#slide");
   // 블릿버튼 : .indic
-  let indic = document.querySelector(".indic");
+  let indic = qs(".indic");
   // console.log(abtn,slide);
+
+  // 슬라이드 (블릿) 개수 상수로 셋팅하기!
+  // 상수는 대문자로 쓰고 단어 구분은 언더바로 함!
+
+  const SLIDE_CNT = 5;
 
   //////////// 초기셋팅하기 ////////
   // 5개의 슬라이드와 블릿을 만들어준다!
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < SLIDE_CNT; i++) {
     // 슬라이드 넣기
     slide.innerHTML += `
-    <li data-seq="${i}">
+    <li ${i === 0 ? 'class="on"' : ""}>
         <img 
         src="images/slide0${i + 1}.jpg"         
         alt="slide">
@@ -67,9 +72,47 @@ function loadFn() {
 
   // li를 생성한 후 그 li다시 수집한다!
   // 블릿의 li까지 수집! indic 변수
-  indic = document.querySelectorAll(".indic li");
+  indic = qsa(".indic li");
 
   // 슬라이드 순번 전역변수
   let snum = 0;
-  
+
+  // 1. 이벤트 연결 설정하기
+  // 대상 : .abtn
+  abtn.forEach((ele) => {
+    addEvt(ele, "click", goSlide);
+  });
+  // addEvt(대상,이벤트,함수);
+
+  // 2. 이벤트 처리 함수 만들기
+  //  [처리 순서]
+  // (1) 오른쪽 버튼이면 전역슬라이드 변수 snum ++
+  // (2) 왼쪽 버튼이면 전역슬라이드 변수 snum --
+  // (3) 이때 한계값을 체크하여 순환되게 함 -> 끝번호 뒤는 첫번호, 첫번호 앞은 끝번호로 순환되게 함
+  // (4) 해당 순번의 슬라이드에 class.on 넣기 -> 나머지 슬라이드는 on 제거하기 (외부 함수로 구성)
+  // (5) 블릿표시자도 슬라이드와 같은 순번에 class on 넣고 나머지는 빼준다 (외부 함수로 구성)
+  // (6) 자동 넘김 구성 함수를 호출하여 인터발 호출 작동 -> 버튼 클릭시 인터발 지우기/일정시간 뒤 다시 인터발 작동
+
+  function goSlide() {
+    // 1. 오른쪽 버튼 여부
+    let isRbtn = this.classList.contains("ab2");
+    /// 호출확인
+    // console.log('오른쪽버튼이니', isRbtn,this);
+
+    // 2. 버튼에 따른 전역 슬라이드 번호 증감하기
+    // (1) 오른쪽 버튼일 경우 증가
+    // 한계 설정: snum이 개수-1과 같으면 첫번호 0
+
+    if (isRbtn) snum === SLIDE_CNT -1 ? (snum = 0) : snum++;
+    // (2) 왼쪽 버튼일 경우 감소
+    // 한계 설정 : snum이 0이면 마지막 순번
+    else snum === 0 ? (snum = SLIDE_CNT - 1) : snum--;
+
+    console.log("snum:" + snum);
+
+
+
+
+
+  } ///////////////// goSlide 함수 /////////////
 } /////////////// loadFn 함수 //////////////
