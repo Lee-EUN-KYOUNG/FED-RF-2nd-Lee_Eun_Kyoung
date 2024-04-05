090721687fda6b427522a353e2017166b45d65d0
EUNKYOUNG <amurca@naver.com>
2024-04-05 16:09:11 +0900

    39회

--- 700.쉐도잉프로젝트/01.DKB_PG/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PG/03.구현소스/js/main.js
@@ -1,5 +1,9 @@
 /* 도깨비 PJ 메인 JS - main.JS */
 
+
+// 나의 함수 불러오기
+import myFn from './my_function.js';
+
 // 부드러운 스크롤 불러오기
 import { startSS, setScrollPos } from "./smoothScroll23.js";
 
@@ -14,5 +18,23 @@ import slideFn from "./slide.js";
 startSS();
 
 // console.log('모듈로 메인 JS 호출!!', document.querySelector('.top-menu'));
+
 /// slideFn 슬라이드 기능 함수 호출
 slideFn();
+
+// 인트로 동영상 파트 클릭시 동영상 태그 넣기
+// 이벤트 대상 === 변경 대상 : .intro-mv-img
+const introMv = myFn.qs('.intro-mv-img');
+introMv.onclick = () => {
+
+    console.log('인트로영상!');
+
+    // 동영상 넣기
+    introMv.innerHTML = `
+    <video src="./images/intro_mv.mp4" autoplay="" controls=""></video>    
+    `;
+
+    // 클래스 off 지우기 (플레이버튼 안나오게 함)
+    introMv.classList.remove('off');
+
+}; ////////////// 클릭 이벤트 함수 //////////////
