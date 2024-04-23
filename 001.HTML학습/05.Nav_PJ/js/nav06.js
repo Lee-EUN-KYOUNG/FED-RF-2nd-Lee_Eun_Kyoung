e15d360b84097e39246038c7ede2f3f976c9b000
EUNKYOUNG <amurca@naver.com>
2024-04-23 15:00:30 +0900

    47

--- 001.HTML학습/05.Nav_PJ/js/nav06.js
+++ 001.HTML학습/05.Nav_PJ/js/nav06.js
@@ -13,7 +13,7 @@ import setSlide from "./drag_slide.js";
 
 // 1. 배너 리스트 셋팅하기
 // (1) 대상 : .slide
-const slideBox = mFn.qs('.slide');
+const slideBox = mFn.qs('.ban-slide');
 
 // 슬라이드 코드 변수 
 let slideCode = '';
