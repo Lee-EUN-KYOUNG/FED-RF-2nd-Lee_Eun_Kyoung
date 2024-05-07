fbef11ce73cc0601c904c8a2274b39f8a1578eb3
EUNKYOUNG <amurca@naver.com>
2024-05-07 12:18:14 +0900

    Update common.js

--- 700.쉐도잉프로젝트/01.DKB_PG/03.구현소스/js/common.js
+++ 700.쉐도잉프로젝트/01.DKB_PG/03.구현소스/js/common.js
@@ -14,7 +14,7 @@ import gnbData from "../data/gnb_data.js";
 // 콤보 박스 데이터 불러오기
 import comboData from "../data/combo_data.js";
 
-console.log(comboData);
+//console.log(comboData);
 
 export default function setElement() {
   // 대상 선정 : #top-area, #ban-area, #spart-menu, #footer-area
@@ -53,7 +53,7 @@ function makeMenu(){
   // -> 키 배열로 변환해서 사용함. 이 객체의 key는 상위 메뉴이기도함
   // Object.keys(객체) -> 해당 객체의 속성명(키) 배열생성
 
-  console.log(Object.keys(gnbData));
+  //console.log(Object.keys(gnbData));
 
   mFn.qs(".gnb").innerHTML = `
 <ul>
@@ -102,7 +102,7 @@ function bindCombo(){
   // 1. 대상 선정 : #brand, #corp
   const brandBox = document.querySelector("#brand");
   const corpBox = document.querySelector("#corp");
-  console.log("콤보바인딩:",brandBox,corpBox);
+  // console.log("콤보바인딩:",brandBox,corpBox);
 
   //  2. 데이터 바인딩하기
   //  2-1. 브랜드 바로가기 콤보 박스 : 단순 바인딩 (option만)
@@ -115,7 +115,32 @@ function bindCombo(){
     <option value="brand${i}">${v}</option>
   `).join('');
   
+  // 2-2. 계열사 바로가기
+  // -> 복합 바인딩 : optgroup > option 
+  // 데이터 분석 :  객체로 된 데이터이므로 map()을 쓰려면 객체의 키(key)를 배열로 추출하여 사용한다
+  // object.keys(객체) -> key 배열
+  // 객체의 값을 사용할 경우는 원본 객체 [키]
 
+
+
+  // 데이터 대상 : ComboData.corp
+  const corpData = Object.keys(comboData.corp);
+  console.log(corpData);
+
+  // 데이터 만들어서 넣기
+  corpBox.innerHTML =
+  corpData.map((v,i)=>`
+  <optgroup label="${v}">
+  ${
+
+    // 해당 객체의 값은 키 배열값과 매칭함
+    // ov 변수는 객체가 가지는 배열값임
+    comboData.corp[v].map((ov,oi) => `
+      <option value="corp${i+1}-${oi+1}">${ov}</option>    
+    `).join('')
+  }
+  </optgroup>
+  `).join('');
 
 
 
