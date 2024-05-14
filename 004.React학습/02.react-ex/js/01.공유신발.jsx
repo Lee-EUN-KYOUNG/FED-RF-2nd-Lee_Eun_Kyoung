e12a6c63c15f8f12e8af5c39d5e798d33d5ac40e
EUNKYOUNG <amurca@naver.com>
2024-05-14 13:12:05 +0900

    58

--- 004.React학습/02.react-ex/js/01.공유신발.jsx
+++ 004.React학습/02.react-ex/js/01.공유신발.jsx
@@ -4,10 +4,13 @@
 // 컴포넌트란? -> 특정 모듈로 구성된 HTML 코드를 리턴하는 객체
 // 함수형 컴포넌트는 첫글자가 대문자인 함수 키워드로 만든다
 
+// 공유 신발 데이터 불러오기
+import guData from "./data/gu_data";
 
-function MainComponent(){
+console.log(guData);
 
-    /****************************************************************** 
+function MainComponent() {
+  /****************************************************************** 
     
                                 [코드 구성]
             1. 타이틀 : h1.tit
@@ -22,40 +25,46 @@ function MainComponent(){
 
     ******************************************************************/
 
-    //// 코드 리턴 구역 /////////
-    return(
-        <React.Fragment>
-
-            {/* 타이틀 */}
-            <h1 className="tit">공유가 신고 다닌다는!</h1>
-            {/* 내용 박스 */}
-            <section>
-                <h2>공유는 오늘도 멋집니다!</h2>
-                <div className="img-box">
-                    <img src="./images/vans/gongyoo.jpg" alt="멋진공유" />
-                </div>
-            </section>
-            {/* 기능 버튼 박스 */}
-            <div className="btn-box">
-                <button>효진초이스 바로가기</button>
-            </div>
-            {/* 상품 리스트 박스 */}
-            <div className="gwrap">
-                
-            </div>
-
-        </React.Fragment>
-    );
-
-
+  //// 코드 리턴 구역 /////////
+  return (
+    <React.Fragment>
+      {/* 타이틀 */}
+      <h1 className="tit">공유가 신고 다닌다는!</h1>
+      {/* 내용 박스 */}
+      <section>
+        <h2>공유는 오늘도 멋집니다!</h2>
+        <div className="img-box">
+          <img src="./images/vans/gongyoo.jpg" alt="멋진공유" />
+        </div>
+      </section>
+      {/* 기능 버튼 박스 */}
+      <div className="btn-box">
+        <button>효진초이스 바로가기</button>
+      </div>
+      {/* 상품 리스트 박스 */}
+      <div className="gwrap">
+        <ul>
+          {guData.map((v) => (
+            <li>
+              <a href="#">
+                <ol class="glist">
+                  <li>
+                    <img
+                      src={"./images/vans/vans_" + v.idx + ".jpg"}
+                      alt="신발"
+                    />
+                  </li>
+                  <li>{v.gname}</li>
+                  <li>가격 : {v.gprice}원</li>
+                </ol>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </React.Fragment>
+  );
 } ///////////////// MainComponent 컴포넌트
 
-
-
-
-
-
-
-
 // 메인 컴포넌트 출력하기
-ReactDOM.render(<MainComponent/>,document.querySelector("#root"));
+ReactDOM.render(<MainComponent />, document.querySelector("#root"));
