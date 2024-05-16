eb61fc1a29a7a368db5986d0225a6f91a587ed3b
EUNKYOUNG <amurca@naver.com>
2024-05-16 12:04:13 +0900

    Update jquery-HrScroll.js

--- 005.jQuery학습/002.실전연습/01_SlideChangeStage/js/jquery-HrScroll.js
+++ 005.jQuery학습/002.실전연습/01_SlideChangeStage/js/jquery-HrScroll.js
@@ -18,6 +18,11 @@ let pgCnt = $(".page").length;
 // 윈도우 가로크기
 let winW, maxLimit;
 
+// 한번에 움직일 스크롤 이동 크기
+const MOVE_NUM = 100;
+// 상수는 모두 대문자로 쓰고 중간에 언더바로 구분한다
+
+
 const chgLimit = () =>{
     
     winW = $(window).width();
@@ -25,10 +30,10 @@ const chgLimit = () =>{
     // 최대 한계값
     maxLimit = (winW*pgCnt) - winW;
 
-    console.log(
+  /*   console.log(
         "window크기:", winW,
         "/페이지수:",pgCnt,
-        "/최대한계값:",maxLimit);
+        "/최대한계값:",maxLimit); */
 }
 
 // 최초 한계값 계산함수 호출
@@ -38,6 +43,10 @@ chgLimit();
 $(window).resize(chgLimit);
 // resize() 메서드 : 리사이즈 이벤트 함수
 
+
+///////////////////////////////////////////////////////////////////////////
+///////////////// 스크롤 이벤트 함수 구현하기
+///////////////////////////////////////////////////////////////////////////
 
 
 
@@ -49,15 +58,23 @@ $(window).resize(chgLimit);
 scTarget.on("wheel", (e)=>{
     
     // 휠 방향 알아내기 (전체 이벤트 객체로부터 얻어옴)
-    let delta = event.wheelDelta;
-    
 
-    if(delta<0) scPos += 200;
-    else scPos -= 200;
+    // (1) event.WheelDelta 값: 기본 이동 100opx + 앞뒤 예비 공백 10px*2=20px
+    // -> 전체 표시 수치 120px을 의미
+    // let delta = event.wheelDelta;
+    // 방향은 마이너스가 아랫 방향
+    // (2) event.deltaY : 기본 이동 -> 기동 이동 크기만 표시함 즉, 100px 이동값 표시
+    // -> 방향은 양수가 아랫방향 wheelDelta보다 나중에 나온 실질적인 표시 객체임
+
+    let delta = event.deltaY;
+    
+    // 방향에 따른 증감은 deltaY는 양수가 아랫방향, WheelDelta는 음수가 아랫방향임
+    if(delta>0) scPos += MOVE_NUM;
+    else scPos -= MOVE_NUM;
 
     // 한계값 체크
     // (1) 최소한계
-    if(scPos<0) scPos = 0;
+    if(scPos<=0) scPos = 0;
     // (2) 최대한계 : 전체 이동 박스 크기 - 화면 가로크기
     if(scPos>=maxLimit) scPos = maxLimit;
 
