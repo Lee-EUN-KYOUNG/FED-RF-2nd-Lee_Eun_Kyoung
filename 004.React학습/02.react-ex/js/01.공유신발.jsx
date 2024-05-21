8f0dce3271a44ffc55f92ed3e02264fe77ef720f
EUNKYOUNG <amurca@naver.com>
2024-05-21 15:38:55 +0900

    60

--- 004.React학습/02.react-ex/js/01.공유신발.jsx
+++ 004.React학습/02.react-ex/js/01.공유신발.jsx
@@ -10,12 +10,9 @@ import GoodsList from "./components/goods_list";
 import GoodsDetails from "./components/goods_detail";
 // 주의 : CDN에서 여기 import 대상은 모두 html 페이지에서 불러와야 사용할 수 있다
 
-
-
 //console.log(guData);
 
 function MainComponent() {
-
   // [후크 상태 관리 변수 셋팅]
   //  1. 리스트 / 상세 보기 전환용 상태 관리 변수 만들기
   const [viewList, setViewList] = React.useState(true);
@@ -23,6 +20,9 @@ function MainComponent() {
   const [idx, setIdx] = React.useState(0);
   // 3. 선택 아이템 고유 이름 상태 관리 변수
   const [selItem, setSelItem] = React.useState("공유");
+  // 4. 테스트용 상태 관리 변수 (의존성 테스트용)
+  const [test, setTest] = React.useState(true);
+
 
   /****************************************************************** 
     
@@ -40,58 +40,95 @@ function MainComponent() {
     ******************************************************************/
   // useEffect 테스트 함수
   const testFn = () => {
-    console.log("테스트중");
-
+    console.log("테스트중 test 상태변수값:",test);
+    // 의존성 테스트를 위한 상태 변수 업데이트 -> true/false값 상호 전환 변경
+    setTest(test?false:true);
   }; /////////////// testFn
 
-  // [1. useEffect : 컴포넌트 생성, 변경, 삭제전 DOM 완성 후 매번 실행되는 코드 구역] 
- React.useEffect(()=>{
+  // [1. useEffect : 의존선 없는 경우]
+  // 컴포넌트 생성, 변경, 삭제전 DOM 완성 후 매번 실행되는 코드 구역
+  React.useEffect(() => {
+    console.log("DOM이 완성되었어");
+  });
+  
+  // [2. useEffect: 의존성 있는 경우]
+  // -> React.useEffect(함수,[의존성 변수])
+  // -> 의존성 변수는 배열 안에 여러 개 셋팅 가능
+  // -> [변수1, 변수2, 변수3]
+  // 공유 초이스와 효진 초이스가 변경될 경우에만 실행하려면?
+  // useState 변수중 원하는 변경에 해당하는 것을 선택하여
+  // 의존성 옵션을 주면 해당 벼누가 변경될 때만 실행하는 랜더링 실행 구역이 만들어진다
+  
+  React.useEffect(()=>{
+    
+    console.log("의존성 useEffect 실행:",selItem);
+    // 글자 커지기 테스트
+    $(".tit span")
+      .css({ display: "inline-block" })
+      .animate({ scale: "200%" }, 1000)
+      .animate({ scale: "100%" }, 1000);
+  }, [selItem, test]);
 
-  console.log("DOM이 완성되었어");
-  // 글자 커지기 테스트
-  $(".tit").animate({fontSize:"50px"},1000)
-  .animate({fontSize:"20px"},1000);
- });
+  // [3. useEffect: 의존성있으나 빈 경우]
+  // -> React.useEffect(함수,[])
+  // -> 최초 로딩시 한번만 실행한다
+  React.useEffect(() => {
+    console.log("useEffect 의존성 비어서 한번만 실행");
+    // 로고 최초 한번만 애니 하기
+    $("#logo")
+    .animate({ scale: "200%",rotate:"360deg" }, 1000)
+    .animate({ scale: "100%",rotate:"0deg" }, 1000);
 
+  }, []);
 
   ///////////////// 코드 리턴 구역 /////////
   return (
     <React.Fragment>
       {/* 타이틀 */}
       <h1 className="tit">
-        {selItem =="공유"?
-        "공유가 신고 다닌다는!":
-        selItem == "효진"?
-        "효진이 입고 다닌다는!":
-        "없음"
-        }
-        </h1>
+        <span>
+          <img
+            id="logo"
+            style={{
+              width: "50px",
+              verticalAlign: "-6px",
+              marginRight: "10px",
+            }}
+            src="./images/logo.png"
+            alt="로고"
+          />
+          {selItem == "공유"
+            ? "공유가 신고 다닌다는!"
+            : selItem == "효진"
+            ? "효진이 입고 다닌다는!"
+            : "없음"}
+        </span>
+      </h1>
       {/* 내용 박스 */}
       <section>
         <h2>
-        {selItem =="공유"?
-        "공유는 오늘도 멋집니다!":
-        selItem == "효진"?
-        "효진은 오늘도 쨍~합니다!":
-        "없음"
-        }
+          {selItem == "공유"
+            ? "공유는 오늘도 멋집니다!"
+            : selItem == "효진"
+            ? "효진은 오늘도 쨍~합니다!"
+            : "없음"}
         </h2>
         <div className="img-box">
-        {selItem =="공유"?
-        <img src="./images/vans/gongyoo.jpg" alt="멋진공유" />:
-        selItem == "효진"?
-        <img src="./images/gallery/hyo.jpg" alt="엘레강스한 효진"/>:
-        ("없음")
-        }
+          {selItem == "공유" ? (
+            <img src="./images/vans/gongyoo.jpg" alt="멋진공유" />
+          ) : selItem == "효진" ? (
+            <img src="./images/gallery/hyo.jpg" alt="엘레강스한 효진" />
+          ) : (
+            "없음"
+          )}
         </div>
       </section>
       {/* 기능 버튼 박스 */}
       <div className="btn-box">
-        <button onClick={()=>
-          setSelItem(selItem=="공유"?"효진":"공유")}
-        >{selItem=="공유"?"효진":"공유"}바로가기
+        <button onClick={() => setSelItem(selItem == "공유" ? "효진" : "공유")}>
+          {selItem == "공유" ? "효진" : "공유"}바로가기
         </button>
-        <br/> <br/>
+        <br /> <br />
         {/* 테스트 버튼 */}
         <button onClick={testFn}>useEffect의존성테스트</button>
       </div>
@@ -99,21 +136,21 @@ function MainComponent() {
       <div className="gwrap">
         {
           // 상태 관리 변수 viewList 값이 true면 리스트 보기
-          viewList?
-          <GoodsList viewDetail={setViewList} updateIdx={setIdx} selItem={selItem}/>:
-          <GoodsDetails backList={setViewList} gNo={idx} selItem={selItem}/>
+          viewList ? (
+            <GoodsList
+              viewDetail={setViewList}
+              updateIdx={setIdx}
+              selItem={selItem}
+            />
+          ) : (
+            <GoodsDetails backList={setViewList} gNo={idx} selItem={selItem} />
+          )
           // false면 상품 상세리스트 보기
         }
-
       </div>
     </React.Fragment>
   );
 } ///////////////// MainComponent 컴포넌트
-
-
-
-
-
 
 // 메인 컴포넌트 출력하기
 ReactDOM.render(<MainComponent />, document.querySelector("#root"));
