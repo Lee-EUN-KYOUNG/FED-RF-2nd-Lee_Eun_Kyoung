8fe3e51407ef541d9597891ffe2700acbd46c147
EUNKYOUNG <amurca@naver.com>
2024-05-21 17:10:09 +0900

    60

--- 004.React학습/02.react-ex/js/01.공유신발.jsx
+++ 004.React학습/02.react-ex/js/01.공유신발.jsx
@@ -10,6 +10,10 @@ import GoodsList from "./components/goods_list";
 import GoodsDetails from "./components/goods_detail";
 // 주의 : CDN에서 여기 import 대상은 모두 html 페이지에서 불러와야 사용할 수 있다
 
+// 공통함수 불러오기
+import * as comFn from "./common/com_fn";
+
+
 //console.log(guData);
 
 function MainComponent() {
@@ -22,7 +26,7 @@ function MainComponent() {
   const [selItem, setSelItem] = React.useState("공유");
   // 4. 테스트용 상태 관리 변수 (의존성 테스트용)
   const [test, setTest] = React.useState(true);
-
+  // 
 
   /****************************************************************** 
     
@@ -45,7 +49,7 @@ function MainComponent() {
     setTest(test?false:true);
   }; /////////////// testFn
 
-  // [1. useEffect : 의존선 없는 경우]
+  // [1. useEffect : 의존성 없는 경우]
   // 컴포넌트 생성, 변경, 삭제전 DOM 완성 후 매번 실행되는 코드 구역
   React.useEffect(() => {
     console.log("DOM이 완성되었어");
@@ -54,32 +58,42 @@ function MainComponent() {
   // [2. useEffect: 의존성 있는 경우]
   // -> React.useEffect(함수,[의존성 변수])
   // -> 의존성 변수는 배열 안에 여러 개 셋팅 가능
+  // -> 의존성 변수는 반드시 상태 관리 변수여야 효과가 있다
   // -> [변수1, 변수2, 변수3]
   // 공유 초이스와 효진 초이스가 변경될 경우에만 실행하려면?
   // useState 변수중 원하는 변경에 해당하는 것을 선택하여
-  // 의존성 옵션을 주면 해당 벼누가 변경될 때만 실행하는 랜더링 실행 구역이 만들어진다
+  // 의존성 옵션을 주면 해당 변수가 변경될 때만 실행하는 랜더링 실행 구역이 만들어진다
   
   React.useEffect(()=>{
     
     console.log("의존성 useEffect 실행:",selItem);
-    // 글자 커지기 테스트
-    $(".tit span")
-      .css({ display: "inline-block" })
-      .animate({ scale: "200%" }, 1000)
-      .animate({ scale: "100%" }, 1000);
+    // 초이스 인트로 애니 함수 호출
+    comFn.choiceIntroAni();
   }, [selItem, test]);
+   
 
   // [3. useEffect: 의존성있으나 빈 경우]
   // -> React.useEffect(함수,[])
   // -> 최초 로딩시 한번만 실행한다
   React.useEffect(() => {
     console.log("useEffect 의존성 비어서 한번만 실행");
-    // 로고 최초 한번만 애니 하기
-    $("#logo")
-    .animate({ scale: "200%",rotate:"360deg" }, 1000)
-    .animate({ scale: "100%",rotate:"0deg" }, 1000);
-
+    
+    // 로고 애니 함수 호출
+    comFn.logoAni();
   }, []);
+
+  // [4. useLayoutEffect: : 화면 업데이트 되기 전 실행 구역]
+  // -> 매번 화면 업데이트시 사용할 경우 의존성을 셋팅하지 않는다
+  // -> 별도로 화면 업데이트 시 특정한 경우에만 사용하기 위해
+  // 의존성 셋팅을 통하여 useEffect와 같은 방법을 사용한다
+  React.useLayoutEffect(()=>{
+    console.log("화면 업데이트 전 실행구역");
+    
+    // 초기화 함수 호출
+    comFn.initFn();
+    
+  },[selItem]);
+
 
   ///////////////// 코드 리턴 구역 /////////
   return (
@@ -106,7 +120,7 @@ function MainComponent() {
       </h1>
       {/* 내용 박스 */}
       <section>
-        <h2>
+        <h2 className="stit">
           {selItem == "공유"
             ? "공유는 오늘도 멋집니다!"
             : selItem == "효진"
@@ -125,7 +139,12 @@ function MainComponent() {
       </section>
       {/* 기능 버튼 박스 */}
       <div className="btn-box">
-        <button onClick={() => setSelItem(selItem == "공유" ? "효진" : "공유")}>
+        <button onClick={() => {
+          // 초이스 종류 변경하기
+          setSelItem(selItem == "공유" ? "효진" : "공유");
+          // 초이스 변경시 무조건 리스트 페이지 보기
+          setViewList(true);
+          }}>
           {selItem == "공유" ? "효진" : "공유"}바로가기
         </button>
         <br /> <br />
