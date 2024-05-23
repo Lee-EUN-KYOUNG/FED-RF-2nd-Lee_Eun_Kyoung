65543498674c5261d25183fe982955a209880d23
EUNKYOUNG <amurca@naver.com>
2024-05-23 13:10:17 +0900

    61

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/main.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/main.js
@@ -1,2 +1,28 @@
 // 보그 PJ 메인 JS - main.js
 
+// 상단영역 불러오기
+import TopArea from "./components/TopArea";
+
+// 메인영역 불러오기
+import MainArea from "./components/MainArea";
+
+// 하단 영역 불러오기
+import FooterArea from "./components/FooterArea";
+
+// 메인 페이지 전체 레이아웃 로딩 컴포넌트
+function Layout() {
+  return (
+    <React.Fragment>
+      {/* 1. 상단 영역 컴포넌트 */}
+      <TopArea />
+      {/* 2. 메인 영역 컴포넌트 */}
+      <MainArea />
+      {/* 3. 하단 영역 컴포넌트 */}
+      <FooterArea />
+    </React.Fragment>
+  );
+} /////////////////////////////// Layout 컴포넌트////////////////
+
+// 메인 페이지 전체 출력하기
+// 상단영역만 넣어보기
+ReactDOM.render(<Layout />, document.querySelector("#root"));
