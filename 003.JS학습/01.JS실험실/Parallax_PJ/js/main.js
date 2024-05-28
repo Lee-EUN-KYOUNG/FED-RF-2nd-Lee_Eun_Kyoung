d1ea72894d88c69554daacb24cf8d874fca884c3
EUNKYOUNG <amurca@naver.com>
2024-05-28 11:14:04 +0900

    Update main.js

--- 003.JS학습/01.JS실험실/Parallax_PJ/js/main.js
+++ 003.JS학습/01.JS실험실/Parallax_PJ/js/main.js
@@ -2,7 +2,7 @@
 
 // 부드러운 스크롤 불러오기
 import { startSS, setPos } from "./smoothScroll20.js";
-// DOM함수 불러오기
+// 내 함수 불러오기
 import mFn from "./my_function.js";
 
 // 부드러운 스크롤 호출
@@ -59,13 +59,14 @@ mFn.addEvt(window, "scroll", scrollFn);
 // 3. 함수 만들기 ////////////////
 // 3-1. 스크롤 이벤트 함수 ////
 function scrollFn() {
-  console.log('스크롤~~~!');
+  //console.log('스크롤~~~!');
 
   // 1. 대상1 : 글자박스 패럴렉스 호출!
-  
+  txtBox.forEach(ele=>moveEl(mFn.getBCR(ele),ele,setH2));
 
   // 2. 대상2 : 아이콘 패럴렉스 호출!
-  
+  icon.forEach(ele=>moveEl(mFn.getBCR(ele),ele,setH1));
+
 } /////////// scrollFn 함수 ////////
 
 // 셋팅값 변수 ////
@@ -73,7 +74,11 @@ function scrollFn() {
 const winH = window.innerHeight;
 // 패럴렉스 범위변수
 const setH1 = 100,
-  setH2 = 200;
+  setH2 = 300;
+
+  // 첫번째 페럴랙스 대상이 이미 화면이 올라와 있어서 초기값 계산하여 위치 조정하기
+  moveEl(mFn.getBCR(txtBox[0]),txtBox[0],setH2)
+  moveEl(mFn.getBCR(icon[0]),icon[0],setH2)
 
 // 3-2. 패럴렉스 이동함수 /////
 function moveEl(elPos, ele, setH) {
@@ -81,14 +86,26 @@ function moveEl(elPos, ele, setH) {
   // (1) elPos - 위치값(getBCL값)
   // (2) ele - 대상요소(패럴렉스대상)
   // (3) setH - 움직일범위수(클수록 빠르게 이동)
-  // console.log(
-  //     '위치:',elPos,
-  //     '\n대상:',ele,
+  //console.log(
+  //    '위치:',elPos,
+  //    '\n대상:',ele,
   //     '\n범위:',setH);
 
   // [ 패럴렉스 범위 : 윈도우 높이값 ~ 0 ]
   // 화면에서 완전히 사라질때 범위는 0보다 작다(약간의 마이너스값)
-  
+  if(elPos < winH && elPos > -200){
+
+     
+      // 1. 위치 이동값 계산
+      let x = setH - ((elPos * setH) / winH)
+      // -(정한 범위 - 실제 이동값) -> 0부터 증가함
+      // 실제이동값 = 위치값*정한범위 / 전체범위
+      // 실제 이동값은 정한 범위에서 빼주고 마이너스를 준다
+
+      // 2. 해당 요소 위치값 이동 CSS 반영
+      ele.style.transform = `translateY(${-x}px)`;
+
+  } ////////////////////// if
 
   /***************************** 
     [ 패럴렉스 위치계산 ]
