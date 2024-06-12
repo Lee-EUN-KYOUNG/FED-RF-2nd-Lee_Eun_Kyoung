82466e082604fe0f21169af27815063e6188e45f
EUNKYOUNG <amurca@naver.com>
2024-06-12 14:37:02 +0900

    65

--- 004.React학습/02.react-ex/js/03.메모이제이션.jsx
+++ 004.React학습/02.react-ex/js/03.메모이제이션.jsx
@@ -1,18 +1,34 @@
 // 03 메모이제이션
 
 function App() {
+  console.log("App컴포넌트 랜더링");
+
   //////////// 점수관련 상태 변수
   const [score, setScore] = React.useState(0);
 
   /// 국적 관련 상태 변수
   const [isKor, setIsKor] = React.useState(true);
 
-  //// 국적 표시 객체
-  const nara = {
+  //// 국적 표시 객체 -> 매번 컴포넌트가 리랜더링 될떄마다 할당되는 객체의 주소값이 변경된다
+  /*  const nara = {
     country: isKor ? "한국" : "일본",
-  };
+  }; */
+
+  // 처음 할당된 객체의 주소값을 메모하여 재사용하면
+  // 다른 상태 변수가 변경될때 같은 주소값을 유지하여
+  // nara 변수가 변경되지 않도록 한다
+  // 사용법 : 변수 = React.useMemo(()=>{return객체})
+  const nara = React.useMemo(() => {
+    return { country: isKor ? "한국" : "일본" };
+  }, [isKor]); ////// isKor에 의존성을 심어준다
+
   ////////////// 랜더링 상태 관리 후크 useEffect 설정
   // nara 객체는 isKor에 의존하고 있음 -> isKor이 변경되면 nara가 변경됨
+
+  // 현재 증상: nara는 isKor에 연결되었으나
+  // score에는 연결되어 있지 않음! 그런데 왜? nara에 변경을 관리하는 useEffect에 걸리는 걸까???
+  //
+
   React.useEffect(() => {
     console.log("nara가 변경됨");
     // 축구 선수 이미지중 해당 나라 이미지가 위로 올라옴
@@ -22,22 +38,35 @@ function App() {
       .animate({ bottom: "+=50px" }, 300);
   }, [nara]);
 
+  /******************************************************** 
+    [흔하면서도 재미있는 현상!!!]
+    useEffect의 의존성 배열에 nara를 넣었는데 
+    score의 state를 변경해도 useEffect가 실행된다.
+    그 이유는 자바스크립트에서 객체는 원시 타입과는 
+    다르게 값이 저장될 때 주소 값으로 저장되기 때문이다.
+    그렇기 때문에 리액트에선 score의 state가 바뀌면 
+    App 컴포넌트가 재호출되면서 nara의 
+    <<주소값이 변경>>이 되었기 때문에 nara가 
+    변경이 되었다고 인식을 한다.
 
-
+    여기서도 useMemo 훅을 통해 이를 방지할 수 있다.
+    -> 방지의 원리는 의존성을 심어서 실제로 변경되는 데이터를
+    특정하여 정확한 반영을 위해 기존데이터를 재사용해준다!
+  ********************************************************/
 
   ///// 코드 리턴구역
   return (
     <header className="header" style={{ textAlign: "center" }}>
+      {/* 제목 서브컴포넌트 */}
+      <ShowTit txt ="한국과 일본이 축구를 하고 있습니다" />
       <h1>
-        한국과 일본이 축구를 하고 있습니다
-        <br />
         {isKor ? "한국이" : "일본이"} 몇 점차로 승리를 예측합니까?
       </h1>
       {/* 점수 입력창 */}
       <input
         type="number"
         value={score}
-        onChange={(e)=>setScore(e.target.value)}
+        onChange={(e) => setScore(e.target.value)}
         style={{
           fontSize: "40px",
           textAlign: "center",
@@ -85,6 +114,29 @@ function App() {
     </header>
   );
 } ////////App 컴포넌트
+
+/// 제목을 뿌리는 서브 컴포넌트
+// -> 초기 셋팅 후 변경 내용이 없는 서브 컴포넌트가 리랜더링 되지 않도록 하는 방법은?
+// --> React.memo(()=>{})
+// 주의 사항 : 컴포넌트가 할당형으로 만들어져야함
+const ShowTit = React.memo(({txt}) =>{
+
+    console.log("ShowTit 컴포넌트 랜더링");
+    return <h1 style={{fontSize:"50px"}}>{txt}</h1>;
+
+}); ///////////////// ShowTit 컴포넌트
+
+/***************************************** 
+    [ 함수형 컴포넌트의 2가지 유형 ]
+
+    1. 선언적 함수형 컴포넌트
+    function 컴포넌트명(){return();}
+
+    2. 화살표 함수형 컴포넌트 (익명함수할당형)
+    const 컴포넌트명 = () => {return();};
+*******************************************/
+
+
 
 /// 리액트 루트 객체 생성
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperVid.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperVid.jsx
@@ -4,6 +4,10 @@ import React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
+// 폰트 어썸 불러오기
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-regular-svg-icons";
+
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
@@ -15,8 +19,6 @@ import "./css/swiper_vid.scss";
 import { swVidData } from "../data/swiper_vid";
 import { Navigation } from "swiper/modules";
 
-
-
 // import required modules
 // 사용할 스와이퍼 모듈을 불러온다
 // (여기서는 네비게이션 - 양쪽 이동 버튼)
@@ -25,9 +27,6 @@ export function SwiperVid({ catName }) {
   // catName -  카테고리명
   // 선택데이터 변수 할당
   const selData = swVidData[catName];
-
-  
-
 
   //// 코드 리턴 구역
   return (
@@ -47,6 +46,16 @@ export function SwiperVid({ catName }) {
               <div className="vid-img">
                 <img src={v.isrc} alt={v.tit} />
                 {/* 폰트어썸 아이콘 */}
+                <FontAwesomeIcon
+                  icon={faCirclePlay}
+                  style={{
+                    position: "absolute",
+                    bottom: "55%",
+                    left: "10%",
+                    color: "#fff",
+                    fontSize: "50px",
+                  }}
+                />
               </div>
               {/* 동영상 타이틀 박스 */}
               <div className="vid-tit">
