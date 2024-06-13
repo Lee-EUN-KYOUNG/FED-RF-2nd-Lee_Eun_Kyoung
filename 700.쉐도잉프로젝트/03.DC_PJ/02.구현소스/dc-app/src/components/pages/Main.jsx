75de3e106356aea7918933cb69dd6c2123898aeb
EUNKYOUNG <amurca@naver.com>
2024-06-13 15:40:07 +0900

    77

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
@@ -1,5 +1,6 @@
 // 메인 페이지 컴포넌트 ///
 import Banner from "../modules/Banner";
+import CatList from "../modules/CatList";
 import SecIntro from "../modules/SecIntro";
 import VidIntro from "../modules/VidIntro";
 import VidSwipe from "../modules/VidSwipe";
@@ -18,6 +19,8 @@ export default function Main(){
             <VidIntro catName="main" clsName="off"/>
             {/* 4. 비디오스와이프 컴포넌트 */}
             <VidSwipe catName="main"/>
+            {/* 5. 캐릭터 리스트 컴포넌트*/}
+            <CatList/>
         </>
     );
 
---
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperCat.jsx
@@ -0,0 +1,77 @@
+// 캐릭터 스와이퍼 플러그인 컴포넌트
+
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// 제이쿼리 넣기
+import $ from "jquery";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// 스와이퍼 비디오 모듈 CSS
+import "./css/swiper_cat.scss";
+
+// 데이터 불러오기
+import { catListData } from "../data/swiper_cat";
+import { Navigation } from "swiper/modules";
+
+// import required modules
+// 사용할 스와이퍼 모듈을 불러온다
+// (여기서는 네비게이션 - 양쪽 이동 버튼)
+
+////////////////////////////// 코딩 구역 //////////////////////////
+export function SwiperCat() {
+  // 선택데이터 변수 할당
+  const selData = catListData;
+
+  //// 코드 리턴 구역
+  return (
+    <>
+      <Swiper
+        //slidesPerView={4}
+        spaceBetween={20}
+        navigation={true}
+        /* 사용할 모듈을 여기에 적용시킨다 */
+        modules={[Navigation]}
+        // 스와이퍼 사이즈별 슬라이드수 변경!
+        breakpoints={{
+          200: {
+            slidesPerView: 3,
+          },
+          700: {
+            slidesPerView: 4,
+          },
+          1000: {
+            slidesPerView: 5,
+          },
+          1200: {
+            slidesPerView: 7,
+          },
+        }}
+        className="mySwiper2"
+      >
+        {selData.map(
+          (v, i) =>
+            /* idx 번호가 고유 번호 7이하만 출력하기 - idx가 문자형이므로 비교를 위해 숫자형으로 바꿔야함 */
+            Number(v.idx) <= 7 && (
+              <SwiperSlide key={i}>
+                <section className="sw-inbox2">
+                  {/* 캐릭터이미지영역 */}
+                  <div className="cat-img2">
+                    <img src={v.tmsrc} alt={v.cname} />
+                  </div>
+                  {/* 캐릭터타이틀영역 */}
+                  <div className="cat-tit2">
+                    <h3>{v.cname}</h3>
+                  </div>
+                </section>
+              </SwiperSlide>
+            )
+        )}
+      </Swiper>
+    </>
+  );
+} /////////// SwiperCat 컴포넌트 ///////////
