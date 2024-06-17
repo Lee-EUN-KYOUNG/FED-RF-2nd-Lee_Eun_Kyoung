dd4c99df9d49365ca6d7d86e2c0f1a0938eb15cc
EUNKYOUNG <amurca@naver.com>
2024-06-17 13:14:50 +0900

    789

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/layout/TopArea.jsx
@@ -3,8 +3,44 @@ import React from "react";
 
 // 메뉴 데이터 불러오기
 import { gnbData } from "../../js/data/gnb";
+import { TotalMenu } from "../modules/TotalMenu";
+
+// 제이쿼리
+import $ from "jquery";
 
 function TopArea(props) {
+  // 전체 메뉴 열기닫기 함수 /////////
+  const showHideMenu = (e) => {
+    //console.log(e.currentTarget);
+    // 전체 메뉴 대상 : .mbox
+    $(".mbox").fadeToggle(300);
+    // fadeIn(); - 서서히 나타남
+    // fadeOut(); - 서서히 사라짐
+    // fadeToggle(); - 서서히 나타남/사라짐 전환함
+
+    // 햄버거 버튼에 클래스 "on" 넣기/빼기
+    // addClass - 클래스 넣기
+    // removeClass - 클래스 빼기
+    // toggleClass - 클래스넣기/빼기
+
+    $(e.currentTarget).toggleClass("on");
+
+    //console.log($(e.currentTarget).is(".on"));
+
+    // 비디오 플레이 재생 /멈춤 하기
+    // 대상 : .bgm
+    let bgm = $(".bgm");
+
+    // 제이쿼리 미디어를 선택 후 실제 사용할때는 get(0)하고 난후 사용한다
+    // 햄버거 버튼에 클래스 "on" 있으면 재생, 없으면 멈춤
+    $(e.currentTarget).is(".on") ? bgm.get(0).play() : bgm.get(0).pause();
+
+    //js에서는 선택후 바로 play(), pause() 사용함
+    // document.querySelector(".bgm").play();
+    // document.querySelector(".bgm").pause();
+  }; ////// showHideMenu ////////////
+
+  ////// 코드 리턴 구역
   return (
     <>
       <div id="top-area">
@@ -24,10 +60,11 @@ function TopArea(props) {
               ))}
             </ul>
           </nav>
-          <div className="ham">
+          <div className="ham" onClick={showHideMenu}>
             <span></span> <span></span> <span></span>
           </div>
           {/* 전체메뉴 컴포넌트 */}
+          <TotalMenu />
         </header>
       </div>
     </>
