7bf6456299d9aed4ee544b50eb2e85ead4018c05
EUNKYOUNG <amurca@naver.com>
2024-07-11 10:27:17 +0900

    1111111111

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
@@ -24,12 +24,27 @@ import CartList from "./components/modules/CartList";
 
 //////////////////////////////////////
 function MainComponent(props) {
+
+  // 로컬스 카트 존재여부 변수
+  let cartTemp = false;
+
+  let localsCart = localStorage.getItem("cart-data");
+  //로컬스 카트 데이터 존재 여부에 따라 상태값 변경
+  if(localsCart){
+
+    // 데이터가 있으면 cartTemp값 true로 업데이트
+    // 데이터 개수가 0이 아니어야함
+    let cartCnt = JSON.parse(localsCart).length;
+    console.log("카트 데이터수:",cartCnt);
+    if(cartCnt > 0) cartTemp = true;
+  }
+
   // 상태 관리 변수 셋팅
   // 1. 페이지 변경 상태 변수
   const [pgName, setPgName] = useState("main");
 
   // 2. 카트 리스트 사용 여부 = true일때 사용
-  const [cartSts, setCartSts] = useState(false);
+  const [cartSts, setCartSts] = useState(cartTemp);
 
 
 
