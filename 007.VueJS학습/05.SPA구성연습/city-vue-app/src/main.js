668f1520576566b0ac1d4d2df1641992652e9a13
EUNKYOUNG <amurca@naver.com>
2024-08-09 11:21:43 +0900

    21334

---
+++ 007.VueJS학습/05.SPA구성연습/city-vue-app/src/main.js
@@ -0,0 +1,15 @@
+// 맨 처음 읽어들이는 JS 파일 - main.js
+
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
