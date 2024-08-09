866d9182fe12e2bb9422af46138c878697ac28ad
EUNKYOUNG <amurca@naver.com>
2024-08-09 13:13:49 +0900

    1111

--- 007.VueJS학습/05.SPA구성연습/city-vue-app/src/App.vue
+++ 007.VueJS학습/05.SPA구성연습/city-vue-app/src/App.vue
@@ -1,22 +1,43 @@
+<!-- 템플릿 구역 -->
 <template>
- <div class="app">
- 
+  <!-- 원래 루트인 #app은 대체된다 -->
+ <div id="app">
+   <TopArea />
+   <MainArea />
+   <FooterArea />
  </div>
 </template>
 
+<!-- 스크립트 구역 -->
 <script>
+// 여기서 import해준다
+import TopArea from './components/TopArea.vue';
+import MainArea from './components/MainArea.vue';
+import FooterArea from './components/FooterArea';
+
+// 내보낼 컴포넌트 이름
 export default {
-  name: 'App'
+    name: 'App',
+    components: { 
+      TopArea, 
+      MainArea,
+      FooterArea
+    }
 }
 </script>
 
+<!-- 스타일 CSS 구역 : scoped 키워드 없으면 전역 -->
 <style>
+/* 최상위 요소 */
 #app {
+  max-width: 500px;
+  margin: 0 auto;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  line-height: 1.6;
+  font-size: 0.9vw;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ 007.VueJS학습/05.SPA구성연습/city-vue-app/src/components/MainArea.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <img v-bind:src="this.$store.state.imgsrc" alt="지역이미지" />
+    <p v-text="this.$store.state.desc"></p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "MainArea",
+  data() {
+    return {
+      msg: "Welcome to Your City",
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+main {
+  padding: 10px;
+}
+main img {
+  width: 100%;
+}
+</style>
